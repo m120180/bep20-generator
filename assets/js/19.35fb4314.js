42b08b5c923b96dfc8e90c193b3c3fd50c549eea
aliberkyurtoglu <aliberkyurtoglu@gmail.com>
2024-06-28 17:24:32 +0300

    publish: Update: Fee list and network list
    generated from commit 1b9b3705ecd781f402586c8ca5cc0b9fee38adf3

--- assets/js/19.35fb4314.js
+++ assets/js/19.35fb4314.js
@@ -1,4 +1,4 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{261:function(t,e,n){
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{262:function(t,e,n){
 /*!
  * vue-carousel v0.18.0-alpha
  * (c) 2019 todd.beauchamp@ssense.com
