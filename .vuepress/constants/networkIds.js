1f431fe0c23554b42eb824e7709e4077ac2e5bdf
aliberkyurtoglu <aliberkyurtoglu@gmail.com>
2024-06-28 17:05:30 +0300

    Create: networkIds constant

---
+++ .vuepress/constants/networkIds.js
@@ -0,0 +1,13 @@
+export const networkIds = {
+    BitciMain: 1907,
+    BitciTest: 1908,
+    ArbitrumMain: 42161,
+    BnbMain: 56
+}
+
+export const networkNames = {
+    BitciMain: 'bitci_main',
+    BitciTest: 'bitci_test',
+    ArbitrumMain: 'arbitrum_main',
+    BnbMain: 'bnb_main'
+}
